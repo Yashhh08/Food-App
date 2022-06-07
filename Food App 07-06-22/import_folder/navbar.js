83a4fd113de11a409a508684c3af146c5c244b9b
Yashhh08 <101392090+Yashhh08@users.noreply.github.com>
2022-06-08 01:28:36 +0530

    Add files via upload

---
+++ Food App 07-06-22/import_folder/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+  return `
+    <div>
+            <a href="index.html">Search</a>
+        </div>
+
+        <div>
+            <a href="day.html">Receipe of the day</a>
+        </div>
+
+        <div>
+            <a href="random.html">Random Receipes</a>
+        </div>`;
+}
+
+export default navbar;
